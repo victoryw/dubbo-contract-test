f9f22bd4dbf916d791c23ff53080218038232c0e
tianyawy <tianya_wy@hotmail.com>
2019-07-29 21:43:31 +0800

    init

---
+++ src/main/java/com/victory/dubbo/contracttest/ContractTestApplication.java
@@ -0,0 +1,13 @@
+package com.victory.dubbo.contracttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractTestApplication.class, args);
+	}
+
+}
